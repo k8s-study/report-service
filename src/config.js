dd8e8afcf4a53d81a4ea83abb7627a7a0feeb2ad
Ssurey Moon <ssureymoon@gmail.com>
2018-05-22 10:40:00 +0900

    add api version prefix to api uri

---
+++ src/config.js
@@ -0,0 +1,1 @@
+exports.API_VER = 'v1';
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const Router = require('koa-router');
 
+const config = require('./config');
 const controllers = require('./controllers');
 
-const router = new Router();
+const router = new Router({
+    prefix: `/${config.API_VER}`,
+});
 router.get('/health', controllers.healthcheck);
 
 module.exports = router;
