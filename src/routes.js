d6e2a702b49364905de84e3251a1d86060947089
Ssurey Moon <ssureymoon@gmail.com>
2018-05-28 01:45:26 +0900

    add results api and its validation

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const Router = require('koa-router');
 
 const config = require('./config');
 const controllers = require('./controllers');
+const validators = require('./validators');
 
 const router = new Router({
     prefix: `/${config.API_VER}`,
 });
 router.get('/health', controllers.healthcheck);
+router.post('/results', validators.results, controllers.results);
 
 module.exports = router;
