0e277f3063ea9a51b8ea8bfd81c16d4080711d69
Ssurey Moon <ssureymoon@gmail.com>
2018-05-29 13:52:41 +0900

    add test for pinged result controller

--- src/tests/controllers.test.js
+++ src/tests/controllers.test.js
@@ -25,3 +25,43 @@ describe('healthcheck controller', async () => {
         expect(controllers.healthcheck(ctx, next)).rejects.toThrowError();
     });
 });
+
+
+describe('pingend results controller', async () => {
+    const next = jest.fn();
+    let ctx;
+
+    beforeEach(() => {
+        ctx = {
+            reports: {
+                save: jest.fn(),
+            },
+        };
+    });
+
+    test('should not save doc and return 400 when input is not valid', async () => {
+        ctx.validation = {
+            isValid: false,
+            messages: {
+                test: 'message',
+            },
+        };
+        await controllers.results(ctx, next);
+        expect(ctx.reports.save).not.toHaveBeenCalled();
+        expect(ctx.status).toEqual(400);
+        expect(ctx.body).toEqual(ctx.validation.messages);
+    });
+
+    test('should save doc and return 201 when input is valid', async () => {
+        ctx.validation = {
+            isValid: true,
+        };
+        ctx.sanitizedBody = {
+            url: 'www.example.com',
+            status: 'good',
+        };
+        await controllers.results(ctx, next);
+        expect(ctx.reports.save).toHaveBeenCalledWith(ctx.sanitizedBody);
+        expect(ctx.status).toEqual(201);
+    });
+});
